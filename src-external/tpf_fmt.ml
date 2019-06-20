7c60576f07e1113f2883e8999de660480e798054
pqwy <dk505@cam.ac.uk>
2019-06-20 08:36:21 +0100

    fmt

--- src-external/tpf_fmt.ml
+++ src-external/tpf_fmt.ml
@@ -2,7 +2,7 @@ open Tpf
 
 include Generic (struct type 'a r = 'a Fmt.t end)
 
-let intersperse pp =
+let sep_by pp =
   let first = ref true in
   fun ppf () -> if !first then first := false else pp ppf ()
 
@@ -11,50 +11,45 @@ open V
 let err_missing_fields s =
   Fmt.invalid_arg
   "Tpf_fmt.g_pp: `%s': some record elements are missing field labels"
-  (meta s).name
+  (v_name s)
+
 let err_extra_fields s =
   Fmt.invalid_arg "Tpf_fmt.g_pp: `%s': extra field labels: %a"
-  (meta s).name Fmt.(list ~sep:comma string)
+  (v_name s) Fmt.(list ~sep:comma string)
 
-let named ppf = Fmt.pf ppf "@[<1>%s@ %a@]"
-let field ppf = Fmt.pf ppf "@[<1>%s =@ %a@]"
+let is_empty s = function [] -> () | fs -> err_extra_fields s fs
 
-let variant ppf pp_rec name = function
-| K _ -> Fmt.string ppf name
-| A (K _, x, pp_x) -> named ppf name !!pp_x x
-| R (K _, x) -> named ppf name pp_rec x
-| s ->
-    let rec go: type a. _ -> (a, _, _) spine Fmt.t = fun sep ppf -> function
-    | K _ -> ()
-    | A (s, x, pp_x) -> go sep ppf s; sep ppf (); !!pp_x ppf x
-    | R (s, x) -> go sep ppf s; sep ppf (); pp_rec ppf x in
-    named ppf name (Fmt.parens (go (intersperse Fmt.comma))) s
-
-let record ppf pp_rec fields =
-  let rec go: type a. _ -> _ -> _ -> (a, _, _) spine -> _ =
-    fun sep fields ppf -> function
-    | K _ -> fields
-    | A (s, x, pp_x) ->
-      ( match go sep fields ppf s with
-        | f::fs -> sep ppf (); field ppf f !!pp_x x; fs
-        | [] -> err_missing_fields s )
-    | R (s, x) ->
-      ( match go sep fields ppf s with
-        | f::fs -> sep ppf (); field ppf f pp_rec x; fs
-        | [] -> err_missing_fields s ) in
-  let pp ppf s = match go (intersperse Fmt.semi) fields ppf s with
-  | [] -> ()
-  | fs -> err_extra_fields s fs in
-  Fmt.braces pp ppf
+let parens pp ppf x = Fmt.(string ppf "("; pp ppf x; string ppf ")")
+let braces pp ppf x = Fmt.(string ppf "{"; pp ppf x; string ppf "}")
 
 let rec g_pp: type a. (a, _) view -> a Fmt.t = fun v ppf x ->
+  let variant ppf s =
+    let rec go: 'a. _ -> ('a, _, _) spine Fmt.t = fun sep ppf -> function
+    | K _ -> ()
+    | A (s, x, pp_x) -> go sep ppf s; sep ppf (); !!pp_x ppf x
+    | R (s, x) -> go sep ppf s; sep ppf (); g_pp v ppf x in
+    match s with
+    | A (K _, x, pp_x) -> !!pp_x ppf x
+    | R (K _, x) -> g_pp v ppf x
+    | s -> parens (go (sep_by Fmt.comma)) ppf s
+  and record fields ppf s =
+    let field ppf s sep pp_x x = function
+    | f::fs -> sep ppf (); Fmt.pf ppf "@[<1>%s =@ %a@]" f pp_x x; fs
+    | [] -> err_missing_fields s in
+    let rec go: 'a. _ -> _ -> _ -> ('a, _, _) spine -> _ =
+      fun sep fields ppf -> function
+    | K _ -> fields
+    | A (s, x, pp_x) -> go sep fields ppf s |> field ppf s sep !!pp_x x
+    | R (s, x) -> go sep fields ppf s |> field ppf s sep (g_pp v) x in
+    let pp_s ppf s = go (sep_by Fmt.semi) fields ppf s |> is_empty s in
+    braces pp_s ppf s in
   let s = v x in
-  let meta = meta s in
-  match meta.fields with
-  | [] -> variant ppf (g_pp v) meta.name s
-  | fields -> match meta.name with
-      | "" -> record ppf (g_pp v) fields s
-      | name -> named ppf name (fun ppf -> record ppf (g_pp v) fields) s
+  let m = v_meta s in
+  match s, m.fields, m.name with
+  | K _, _     , name -> Fmt.string ppf name
+  | _  , []    , name -> Fmt.pf ppf "@[<1>%s@ %a@]" name variant s
+  | _  , fields, ""   -> record fields ppf s
+  | _  , fields, name -> Fmt.pf ppf "@[<1>%s@ %a@]" name (record fields) s
 
 (* Either *)
 let g_pp0 g0 = v0 g_pp g0
