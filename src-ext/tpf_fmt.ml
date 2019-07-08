cfa86efa25d4412cb7e56d053c5aebd39da5f9b1
pqwy <dk505@cam.ac.uk>
2019-07-08 16:01:30 +0100

    redo tpf_fmt

--- src-ext/tpf_fmt.ml
+++ src-ext/tpf_fmt.ml
@@ -3,44 +3,51 @@
 
 open Tpf
 
-let sep_by pp =
-  let first = ref true in
-  fun ppf () -> if !first then first := false else pp ppf ()
-
 module G = Generic (struct type 'a q = 'a Fmt.t end)
 open G
 open V
 
-let parens pp ppf x = Fmt.(string ppf "("; pp ppf x; string ppf ")")
+let pp_pp ppf pp = pp ppf ()
+let sum name ppf = function
+| [] -> Fmt.string ppf name
+| [x] -> Fmt.pf ppf "@[<1>%s@ %a@]" name x ()
+| xs -> Fmt.(pf ppf "@[<1>%s@ (%a)@]" name (list ~sep:comma pp_pp) xs)
 
-let rec g_pp: 'a. ('a, _) view -> 'a Fmt.t = fun v ppf x ->
-  let variant ppf s =
-    let sep = sep_by Fmt.comma in
-    let rec go: 'a. ('a, _, _) spine Fmt.t = fun ppf -> function
-    | K _ -> ()
-    | A (s, a, f) -> go ppf s; sep ppf (); !:f ppf a
-    | R (s, a) -> go ppf s; sep ppf (); g_pp v ppf a in
-    match s with
-    | A (K _, a, f) -> !:f ppf a
-    | R (K _, a) -> g_pp v ppf a
-    | s -> parens go ppf s
-  and record m ppf s =
-    let sep = sep_by Fmt.semi in
-    let field ppf i pp_x x =
-      sep ppf (); Fmt.pf ppf "@[<1>%s =@ %a@]" (field m i) pp_x x in
-    let rec go: 'a. _ -> _ -> ('a, _, _) spine -> _ =
-      fun i ppf -> function
-    | K _ -> ()
-    | A (s, a, f) -> go (i - 1) ppf s; field ppf i !:f a
-    | R (s, a) -> go (i - 1) ppf s; field ppf i (g_pp v) a in
-    let pp_s ppf s = go (fields m - 1) ppf s in
-    pp_s ppf s in
+let record name ppf xs =
+  let pp_kv ppf (field, pp) = Fmt.pf ppf "@[<1>%s =@ %a@]" field pp () in
+  match name with
+  | Some n -> Fmt.(pf ppf "@[<1>%s@ {%a}@]" n (list ~sep:semi pp_kv) xs)
+  | None -> Fmt.(pf ppf "@[<1>{%a}@]" (list ~sep:semi pp_kv) xs)
+
+let rec g_pp ?(sum = sum) ?(record = record) v ppf x =
+  let goto10 = g_pp ~sum ~record v in
+  let rec go_v: 'a. ('a, _, _) spine -> _ = fun s acc -> match s with
+  | K _ -> acc
+  | A (s, a, af) -> go_v s (Fmt.const !:af a :: acc)
+  | R (s, a) -> go_v s (Fmt.const goto10 a :: acc) in
+  let rec go_r: 'a. ('a, _, _) spine -> _ = fun s m i acc -> match s with
+  | K _ -> acc
+  | A (s, a, af) -> go_r s m (i - 1) ((field m i, Fmt.const !:af a) :: acc)
+  | R (s, a) -> go_r s m (i - 1) ((field m i, Fmt.const goto10 a) :: acc) in
   let m = meta v x in
   match spine v x, fields m, name m with
-  | K _, _, name -> Fmt.string ppf name
-  | s  , 0, name -> Fmt.pf ppf "@[<1>%s@ %a@]" name variant s
-  | s  , _, ""   -> Fmt.pf ppf "@[<1>{%a}@]" (record m) s
-  | s  , _, name -> Fmt.pf ppf "@[<1>%s@ %a@]" name (record m) s
+  | K _, _, name -> sum name ppf []
+  | s  , 0, name -> sum name ppf (go_v s [])
+  | s  , _, ""   -> record None ppf (go_r s m (fields m - 1) [])
+  | s  , _, name -> record (Some name) ppf (go_r s m (fields m - 1) [])
+
+type sum = string -> unit Fmt.t list Fmt.t
+type record = string option -> (string * unit Fmt.t) list Fmt.t
 
 include P
-include View (struct type 'a r = 'a Fmt.t let gfun = g_pp end)
+
+let data0 ?sum ?record (d: _ data0) = app0 (g_pp ?sum ?record) d.view
+let data1 ?sum ?record (d: _ data1) = app1 (g_pp ?sum ?record) d.view
+let data2 ?sum ?record (d: _ data2) = app2 (g_pp ?sum ?record) d.view
+let data3 ?sum ?record (d: _ data3) = app3 (g_pp ?sum ?record) d.view
+let data4 ?sum ?record (d: _ data4) = app4 (g_pp ?sum ?record) d.view
+let data5 ?sum ?record (d: _ data5) = app5 (g_pp ?sum ?record) d.view
+let data6 ?sum ?record (d: _ data6) = app6 (g_pp ?sum ?record) d.view
+let data7 ?sum ?record (d: _ data7) = app7 (g_pp ?sum ?record) d.view
+let data8 ?sum ?record (d: _ data8) = app8 (g_pp ?sum ?record) d.view
+let data9 ?sum ?record (d: _ data9) = app9 (g_pp ?sum ?record) d.view
--- src-ext/tpf_fmt.mli
+++ src-ext/tpf_fmt.mli
@@ -1,13 +1,63 @@
 (* Copyright (c) 2019 David Kaloper Meršinjak. All rights reserved.
    See LICENSE.md. *)
 
-(** Generic printer *)
+(** Generic printers *)
 
 open Tpf
 
-include P with type 'a q := 'a Fmt.t
-val g_pp: ('a, p) Tpf.view -> 'a Fmt.t
-(** [g_pp v ppf x] pretty-prints [x], viewed by [v], on [ppf], mimicking the
-    toplevel printer. *)
+type sum = string -> unit Fmt.t list Fmt.t
+(** [sum name args] formats a variant with constructor [name] and
+    argument formatters [args]. *)
 
-include Data with type 'a q := 'a Fmt.t and type 'a r := 'a Fmt.t
+type record = string option -> (string * unit Fmt.t) list Fmt.t
+(** [record None fields] formats a plain constructorless record;
+    [record (Some name) fields] formats an inline record with constructor
+    [name]. [args] are argument formatters paired with field names. *)
+
+include P with type 'a q := 'a Fmt.t
+
+val g_pp : ?sum:sum -> ?record:record -> ('a, p) view -> 'a Fmt.t
+(** [g_pp ~sum ~record v ppf x] uses [v] to view [x] and pretty-print it on
+    [ppf].
+
+    [sum] controls the formatting of plain variants, [record] of records.
+    Defaults match the way toplevel prints values. *)
+
+(* Note - this blaaargh can be replaced by including
+   [Data with type 'a r := ?sum -> ?record -> 'a Fmt.t] but we pull the optional
+   arguments to the beginning for a nicer API. *)
+
+val data0 : ?sum:sum -> ?record:record ->
+            'a data0 ->
+            'a Fmt.t
+val data1 : ?sum:sum -> ?record:record ->
+            ('a, 'b) data1 ->
+            'a Fmt.t -> 'b Fmt.t
+val data2 : ?sum:sum -> ?record:record ->
+            ('a, 'b, 'c) data2 ->
+            'a Fmt.t -> 'b Fmt.t -> 'c Fmt.t
+val data3 : ?sum:sum -> ?record:record ->
+            ('a, 'b, 'c, 'd) data3 ->
+            'a Fmt.t -> 'b Fmt.t -> 'c Fmt.t -> 'd Fmt.t
+val data4 : ?sum:sum -> ?record:record ->
+            ('a, 'b, 'c, 'd, 'e) data4 ->
+            'a Fmt.t -> 'b Fmt.t -> 'c Fmt.t -> 'd Fmt.t -> 'e Fmt.t
+val data5 : ?sum:sum -> ?record:record ->
+            ('a, 'b, 'c, 'd, 'e, 'f) data5 ->
+            'a Fmt.t -> 'b Fmt.t -> 'c Fmt.t -> 'd Fmt.t -> 'e Fmt.t -> 'f Fmt.t
+val data6 : ?sum:sum -> ?record:record ->
+            ('a, 'b, 'c, 'd, 'e, 'f, 'g) data6 ->
+            'a Fmt.t -> 'b Fmt.t -> 'c Fmt.t -> 'd Fmt.t -> 'e Fmt.t ->
+            'f Fmt.t -> 'g Fmt.t
+val data7 : ?sum:sum -> ?record:record ->
+            ('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h) data7 ->
+            'a Fmt.t -> 'b Fmt.t -> 'c Fmt.t -> 'd Fmt.t -> 'e Fmt.t ->
+            'f Fmt.t -> 'g Fmt.t -> 'h Fmt.t
+val data8 : ?sum:sum -> ?record:record ->
+            ('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i) data8 ->
+            'a Fmt.t -> 'b Fmt.t -> 'c Fmt.t -> 'd Fmt.t -> 'e Fmt.t ->
+            'f Fmt.t -> 'g Fmt.t -> 'h Fmt.t -> 'i Fmt.t
+val data9 : ?sum:sum -> ?record:record ->
+            ('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j) data9 ->
+            'a Fmt.t -> 'b Fmt.t -> 'c Fmt.t -> 'd Fmt.t -> 'e Fmt.t ->
+            'f Fmt.t -> 'g Fmt.t -> 'h Fmt.t -> 'i Fmt.t -> 'j Fmt.t
